0e9cada9fb6f2a56c8147846187198cb7f1e5463
Artur-1995 <107651851+Artur-1995@users.noreply.github.com>
2023-04-19 19:20:30 +0300

    Create CreateStructure.sql

---
+++ CreateStructure.sql
@@ -0,0 +1,13 @@
+-- Database: sql_test
+
+-- DROP DATABASE IF EXISTS sql_test;
+
+CREATE DATABASE sql_test
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
